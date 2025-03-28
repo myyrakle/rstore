985b99ea3acc5ace8ef195b66cde9bd17a46ce64
myyrakle <sssang97@naver.com>
2025-03-28 22:32:20 +0900

    [#1] axum 기본 구조 초기화

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(health_check));
+
+    let addr = "0.0.0.0:3000";
+    println!("Listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check() -> impl IntoResponse {
+    Response::builder()
+        .status(StatusCode::OK)
+        .body("OK".to_string())
+        .unwrap()
 }
