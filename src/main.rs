007576c32d5b7102d67b3dff153be0d6d71eea39
myyrakle <sssang97@naver.com>
2025-04-01 01:59:02 +0900

    [#2] set_value API 구현

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
 use axum::{
-    Router,
+    Json, Router,
+    extract::State,
     http::StatusCode,
     response::{IntoResponse, Response},
-    routing::get,
+    routing::{get, post},
 };
+
+type KVData = Arc<Mutex<HashMap<String, String>>>;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(health_check));
+    let state = Arc::new(Mutex::new(HashMap::<String, String>::new()));
+
+    let app = Router::new()
+        .route("/", get(health_check))
+        .route("/value", post(set_value))
+        .with_state(state);
 
     let addr = "0.0.0.0:3000";
     println!("Listening on {}", addr);
@@ -21,4 +34,18 @@ async fn health_check() -> impl IntoResponse {
         .status(StatusCode::OK)
         .body("OK".to_string())
         .unwrap()
+}
+
+#[derive(serde::Deserialize)]
+struct SetValueRequest {
+    key: String,
+    value: String,
+}
+
+async fn set_value(state: State<KVData>, Json(body): Json<SetValueRequest>) -> impl IntoResponse {
+    let mut state = state.lock().unwrap();
+
+    state.insert(body.key, body.value);
+
+    StatusCode::NO_CONTENT
 }
